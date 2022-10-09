4b8ff58824116e0667cac7604bf97be6d342e774
mariusfa <mariusfagerland@yahoo.no>
2022-10-09 13:10:40 +0200

    Fix warning

--- src/features/toggles/Toggles.tsx
+++ src/features/toggles/Toggles.tsx
@@ -94,6 +94,7 @@ export const Toggles: React.FC = () => {
                                 id={`toggle-${toggle.id}`}
                                 className='sr-only peer'
                                 checked={toggle.isToggled}
+                                readOnly
                             />
                             <div className='w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full after:absolute after:top-[4px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600'></div>
                         </label>
