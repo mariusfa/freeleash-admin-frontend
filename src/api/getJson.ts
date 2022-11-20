89eac7ab2cac265aa0901b38142c8e36e31756c0
mariusfa <mariusfagerland@yahoo.no>
2022-11-20 23:20:16 +0100

    Throw error when error

--- src/api/getJson.ts
+++ src/api/getJson.ts
@@ -1,5 +1,8 @@
 export const getJson = async (url: string) => {
     const response = await fetch(url)
+    if (!response.ok) {
+        throw Error(`Fetch failed for ${url}`)
+    }
     return await response.json()
 }
 
