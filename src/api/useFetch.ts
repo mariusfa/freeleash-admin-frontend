1363f448b173c2b373cd399b258e32a167dc8b4f
mariusfa <mariusfagerland@yahoo.no>
2022-11-23 22:53:50 +0100

    Update fetch

--- src/api/useFetch.ts
+++ src/api/useFetch.ts
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 import { getJson } from './getJson';
 
-export const useFetch = (url: string) => {
-    const [data, setData] = useState(null);
+export const useFetch = <T>(
+    url: string,
+    initData: T,
+    intervalTime?: number
+): { data: T; isError: boolean; refetch: () => void; isLoading: boolean } => {
+    const [data, setData] = useState<T>(initData);
     const [isError, setIsError] = useState(false);
     const [refetch, setRefetch] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
+            console.log('fetching');
             const { data: result, error } = await getJson(url);
             setData(result);
             setIsError(!!error);
@@ -16,6 +21,14 @@ export const useFetch = (url: string) => {
             setIsLoading(false);
         };
         getData();
+
+        if (intervalTime) {
+            const interval = setInterval(() => {
+                getData();
+                console.log('hello');
+            }, intervalTime);
+            return () => clearInterval(interval);
+        }
     }, [refetch]);
 
     return { data, isError, isLoading, refetch: () => setRefetch(true) };
