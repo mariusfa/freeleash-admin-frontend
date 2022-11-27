22394dad5f32e8f796b993dd5a8a69e1bbcda789
mariusfa <mariusfagerland@yahoo.no>
2022-11-27 12:43:15 +0100

    Update Edit team

--- src/features/teams/EditTeam.tsx
+++ src/features/teams/EditTeam.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { TeamContext } from './TeamContextProvider';
 import { Form, Field } from 'react-final-form';
-import { putJson } from '../../api';
+import { useSendData } from '../../api';
 import { Heading1, InputText, Label, PrimaryButton } from '../../components';
 
 export const EditTeam: React.FC = () => {
@@ -10,20 +10,28 @@ export const EditTeam: React.FC = () => {
     const { teams, refetch } = useContext(TeamContext);
     const team = teams.find((team) => team.name === teamName);
     const navigate = useNavigate();
+    const { sendData, isError, isSubmitting } = useSendData();
 
     if (team?.id === undefined) {
         return <p>Could not find team by team name: {teamName}</p>;
     }
 
     const onSubmit = async (values: any) => {
-        await putJson(`http://localhost:8080/team/${team?.id}`, values);
-        refetch();
-        navigate(`/${values.name}/toggles`);
+        const { error } = await sendData(
+            `http://localhost:8080/team/${team?.id}`,
+            'PUT',
+            values
+        );
+        if (!error) {
+            refetch();
+            navigate(`/${values.name}/toggles`);
+        }
     };
 
     return (
         <>
             <Heading1>Edit team</Heading1>
+            {isError && <div>Error updating team</div>}
             <Form
                 initialValues={{ name: team.name }}
                 onSubmit={onSubmit}
@@ -33,7 +41,9 @@ export const EditTeam: React.FC = () => {
                         <Field name='name'>
                             {({ input }) => <InputText id='name' {...input} />}
                         </Field>
-                        <PrimaryButton>Edit team</PrimaryButton>
+                        <PrimaryButton disabled={isSubmitting}>
+                            {isSubmitting ? 'Submitting...' : 'Edit team'}
+                        </PrimaryButton>
                     </form>
                 )}
             />
